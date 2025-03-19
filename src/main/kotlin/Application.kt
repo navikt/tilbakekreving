a0e5d5b64cd694955c2fd126c7c3d7416bfbe586
Odd Gunnar <oddgunnarfatland@gmail.com>
2025-03-19 11:53:50 +0100

    Init Ktor

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,12 @@
+package no.nav
+
+import io.ktor.server.application.Application
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain
+        .main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
