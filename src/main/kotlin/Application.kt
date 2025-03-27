9d463ea06f731c5e250677ef52cdcfd2e56d9ed4
Odd Gunnar <oddgunnarfatland@gmail.com>
2025-03-27 10:41:04 +0100

    Legg til liveness og readiness routes

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,10 +1,16 @@
 package no.nav
 
 import io.ktor.server.application.Application
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain
-        .main(args)
+    embeddedServer(
+        factory = Netty,
+        port = 8080,
+        host = "0.0.0.0",
+        module = Application::module,
+    ).start(wait = true)
 }
 
 fun Application.module() {
