a03ad94ed78172968d122be540b4b954d40beddc
Odd Gunnar <oddgunnarfatland@gmail.com>
2025-05-20 12:59:54 +0200

    Fjerner configureRouting-funksjonen og flytter helsesjekker direkte til Application-oppstart

--- src/main/kotlin/no/nav/tilbakekreving/Application.kt
+++ src/main/kotlin/no/nav/tilbakekreving/Application.kt
@@ -1,14 +1,17 @@
 package no.nav.tilbakekreving
 
 import io.ktor.client.engine.cio.CIO
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.log
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.server.response.respond
+import io.ktor.server.routing.get
 import io.ktor.server.routing.route
+import io.ktor.server.routing.routing
 import no.nav.tilbakekreving.infrastructure.client.maskinporten.TexasMaskinportenClient
 import no.nav.tilbakekreving.infrastructure.client.skatteetaten.SkatteetatenInnkrevingsoppdragHttpClient
-import no.nav.tilbakekreving.infrastructure.route.configureRouting
 import no.nav.tilbakekreving.infrastructure.route.hentKravdetaljerRoute
 import no.nav.tilbakekreving.infrastructure.route.hentKravoversikt
 import no.nav.tilbakekreving.plugin.MaskinportenAuthHeaderPlugin
@@ -43,13 +46,19 @@ fun Application.module() {
         SkatteetatenInnkrevingsoppdragHttpClient(tilbakekrevingConfig.skatteetaten.baseUrl, skatteetatenClient)
 
     configureSerialization()
-    configureRouting {
+    routing {
         route("/internal") {
             route("/kravdetaljer") {
                 hentKravdetaljerRoute(innkrevingsoppdragHttpClient)
             }
             route("/kravoversikt") {
                 hentKravoversikt(innkrevingsoppdragHttpClient)
+            }
+            get("/isAlive") {
+                call.respond<HttpStatusCode>(HttpStatusCode.OK)
+            }
+            get("/isReady") {
+                call.respond<HttpStatusCode>(HttpStatusCode.OK)
             }
         }
     }
