264cdce770e700537d9b301b122a83dfee8f00f4
Odd Gunnar <oddgunnarfatland@gmail.com>
2025-07-02 10:50:15 +0200

    Ekstraher token-verifisering til egen `configureAuthentication`-funksjon.

--- src/main/kotlin/no/nav/tilbakekreving/Application.kt
+++ src/main/kotlin/no/nav/tilbakekreving/Application.kt
@@ -1,32 +1,27 @@
 package no.nav.tilbakekreving
 
-import arrow.core.getOrElse
 import io.ktor.client.engine.cio.CIO
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
-import io.ktor.server.application.install
 import io.ktor.server.application.log
-import io.ktor.server.auth.Authentication
 import io.ktor.server.auth.authenticate
-import io.ktor.server.auth.bearer
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
-import no.nav.tilbakekreving.infrastructure.client.AccessTokenVerifier
 import no.nav.tilbakekreving.infrastructure.client.TexasClient
 import no.nav.tilbakekreving.infrastructure.client.maskinporten.TexasMaskinportenClient
 import no.nav.tilbakekreving.infrastructure.client.skatteetaten.SkatteetatenInnkrevingsoppdragHttpClient
 import no.nav.tilbakekreving.infrastructure.route.hentKravdetaljerRoute
 import no.nav.tilbakekreving.infrastructure.route.hentKravoversikt
 import no.nav.tilbakekreving.plugin.MaskinportenAuthHeaderPlugin
+import no.nav.tilbakekreving.setup.configureAuthentication
 import no.nav.tilbakekreving.setup.configureCallLogging
 import no.nav.tilbakekreving.setup.configureSerialization
 import no.nav.tilbakekreving.setup.createHttpClient
 import no.nav.tilbakekreving.setup.loadConfiguration
-import org.slf4j.LoggerFactory
 
 fun main() {
     embeddedServer(
@@ -62,28 +57,7 @@ fun Application.module() {
     val accessTokenVerifier = TexasClient(httpClient, tilbakekrevingConfig.nais.naisTokenIntrospectionEndpoint)
     configureSerialization()
     configureCallLogging(appEnv)
-    install(Authentication) {
-        val logger = LoggerFactory.getLogger("Authentication")
-        bearer("entra-id") {
-            authenticate { credentials ->
-                accessTokenVerifier
-                    .verifyToken(credentials.token)
-                    .getOrElse { error ->
-                        when (error) {
-                            is AccessTokenVerifier.VerificationError.FailedToVerifyToken -> {
-                                logger.error("Token verification failed: $error")
-                                null
-                            }
-
-                            is AccessTokenVerifier.VerificationError.InvalidToken -> {
-                                logger.error("Token is invalid: $error")
-                                null
-                            }
-                        }
-                    }?.groupIds
-            }
-        }
-    }
+    configureAuthentication(accessTokenVerifier)
 
     routing {
         route("/internal") {
---
+++ src/main/kotlin/no/nav/tilbakekreving/setup/ConfigureAuthentication.kt
@@ -0,0 +1,34 @@
+package no.nav.tilbakekreving.setup
+
+import arrow.core.getOrElse
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.auth.Authentication
+import io.ktor.server.auth.bearer
+import no.nav.tilbakekreving.infrastructure.client.AccessTokenVerifier
+import org.slf4j.LoggerFactory
+
+fun Application.configureAuthentication(accessTokenVerifier: AccessTokenVerifier) {
+    install(Authentication) {
+        val logger = LoggerFactory.getLogger("Authentication")
+        bearer("entra-id") {
+            authenticate { credentials ->
+                accessTokenVerifier
+                    .verifyToken(credentials.token)
+                    .getOrElse { error ->
+                        when (error) {
+                            is AccessTokenVerifier.VerificationError.FailedToVerifyToken -> {
+                                logger.error("Token verification failed: $error")
+                                null
+                            }
+
+                            is AccessTokenVerifier.VerificationError.InvalidToken -> {
+                                logger.error("Token is invalid: $error")
+                                null
+                            }
+                        }
+                    }?.groupIds
+            }
+        }
+    }
+}
